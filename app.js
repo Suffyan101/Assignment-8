6ac05c1f053874b431e276426d1decaa66bdaab0
Suffyan101 <suffyan.r01@gmail.com>
2022-06-27 23:20:20 +0500

    Assignment 8

---
+++ app.js
@@ -0,0 +1,67 @@
+// Chapter # 26-30 
+
+// Question 1
+
+// var a =  prompt("Please enter Positive number")
+
+// document.write("Number: "+ a+"<br>")
+
+// document.write("Round Off value: "+ Math.round(a)+"<br>")
+// document.write("Floor value: "+ Math.floor(a)+"<br>")
+// document.write("Round Ceil Value: "+ Math.ceil(a))
+
+
+// Question 2
+
+// var a =  prompt("Please enter negative number")
+
+// document.write("Number: "+ a+"<br>")
+
+// document.write("Round Off value: "+ Math.round(a)+"<br>")
+
+// document.write("Floor value: "+ Math.floor(a)+"<br>")
+
+// document.write("Round Ceil Value: "+ Math.ceil(a))
+
+
+// Question 3
+
+// var a = prompt("Please enter positive or negative number to display absolute number")
+
+
+// document.write("User Enter Value is: "+ a+"<br>")
+
+// document.write("Absolute value of user value: "+ Math.abs(a))
+
+
+// Question 4
+
+// var a = Math.ceil(Math.random()*6)
+
+// document.write("<h3>"+"Dice Number is: "+"</h3>"+"<br>")
+
+// document.write("<h1>"+ "Random Dice Value"+ a + "</h1>")
+
+
+// Question 5
+
+// var a = Math.ceil(Math.random()*2)
+
+// if( a === 1){
+//     document.write(a+"<br>")
+//     document.write("Random Coin value: Tails")
+// }
+// else{
+//     document.write(a+"<br>")
+//     document.write("Random Coin value: Heads")
+// }
+
+
+// Question 6
+
+// var a = Math.ceil(Math.random()*100)
+
+// document.write("Random number between 1 and 100: "+ a)
+
+// Question 7
+
